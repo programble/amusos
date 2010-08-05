3195b1cd6113a856aa37a801b88d3b0165be8e34
Curtis (Programble) <programble@gmail.com>
2010-08-05 00:16:46 -0400

    Added some lib functions

---
+++ lib/include/common.h
@@ -0,0 +1,37 @@
+/*  Copyright 2010 Curtis McEnroe <programble@gmail.com>
+ *
+ *  This file is part of AmusOS.
+ *
+ *  AmusOS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  AmusOS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with AmusOS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define STRING(x) #x
+#define MACRO_STRING(x) STRING(x)
+
+#define NULL ((void*) 0)
+
+#define SYSCALL_INTERRUPT 0x80
+
+typedef enum
+{
+    false,
+    true
+} bool;
+
+typedef char* string;
+
+#endif
---
+++ lib/src/text.c
@@ -0,0 +1,30 @@
+/*  Copyright 2010 Curtis McEnroe <programble@gmail.com>
+ *
+ *  This file is part of AmusOS.
+ *
+ *  AmusOS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  AmusOS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with AmusOS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <text.h>
+
+void putch(char c)
+{
+    __asm__ __volatile__("mov $0, %%eax; mov %0, %%ebx; int $" MACRO_STRING(SYSCALL_INTERRUPT) ";" : : "m" (c) : "eax", "ebx");
+}
+
+void puts(string s)
+{
+    for (char *c = s; c != 0x0; c++)
+        putch(*c);
+}
