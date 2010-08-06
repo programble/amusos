826d8d2aa35b8f72591e729e0769baa8f10ad52a
Curtis (Programble) <programble@gmail.com>
2010-08-06 00:06:18 -0400

    Testing, WTF?

--- games/test/src/main.c
+++ games/test/src/main.c
@@ -20,9 +20,8 @@
 
 void _start()
 {
-    cls();
-    for (int i = 0; i < 80 * 19; i++)
-        puts("a");
+    set_cursor_position(14, 11);
+    puts("ABBA");
     char text[] = "RAINBOW";
     color fore[] = { blue, green, cyan, red, magenta, blue, green };
     color back[] = { magenta, red, cyan, green, blue, magenta, red };
