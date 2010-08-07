23bdf5718575c80d9412f50e4ed56064b6b2a240
Curtis (Programble) <programble@gmail.com>
2010-08-07 16:22:43 -0400

    Macro tweak

--- lib/include/syscall.h
+++ lib/include/syscall.h
@@ -23,12 +23,12 @@
 
 #define SYSCALL_INTERRUPT 0x80
 
-#define syscall0(x) __asm__ __volatile__("mov $" MACRO_STRING(x) ", %%eax; int $" MACRO_STRING(SYSCALL_INTERRUPT) ";" : : : "eax")
+#define syscall0(x) __asm__ __volatile__("mov $" #x ", %%eax; int $" MACRO_STRING(SYSCALL_INTERRUPT) ";" : : : "eax")
 
-#define syscall1(x, y) __asm__ __volatile__("mov $" MACRO_STRING(x) ", %%eax; mov %0, %%ebx; int $" MACRO_STRING(SYSCALL_INTERRUPT) ";" : : "m" (y) : "eax", "ebx")
+#define syscall1(x, y) __asm__ __volatile__("mov $" #x ", %%eax; mov %0, %%ebx; int $" MACRO_STRING(SYSCALL_INTERRUPT) ";" : : "m" (y) : "eax", "ebx")
 
-#define syscall2(x, y, z) __asm__ __volatile__("mov $" MACRO_STRING(x) ", %%eax; mov %0, %%ebx; mov %1, %%ecx; int $" MACRO_STRING(SYSCALL_INTERRUPT) ";" : : "m" (y), "m" (z) : "eax", "ebx", "ecx")
+#define syscall2(x, y, z) __asm__ __volatile__("mov $" #x ", %%eax; mov %0, %%ebx; mov %1, %%ecx; int $" MACRO_STRING(SYSCALL_INTERRUPT) ";" : : "m" (y), "m" (z) : "eax", "ebx", "ecx")
 
-#define syscall3(a, b, c, d) __asm__ __volatile__("mov $" MACRO_STRING(a) ", %%eax; mov %0, %%ebx; mov %1, %%ecx; mov %2, %%edx; int $" MACRO_STRING(SYSCALL_INTERRUPT) ";" : : "m" (b), "m" (c), "m" (d) : "eax", "ebx", "ecx", "edx")
+#define syscall3(a, b, c, d) __asm__ __volatile__("mov $" #a ", %%eax; mov %0, %%ebx; mov %1, %%ecx; mov %2, %%edx; int $" MACRO_STRING(SYSCALL_INTERRUPT) ";" : : "m" (b), "m" (c), "m" (d) : "eax", "ebx", "ecx", "edx")
 
 #endif
