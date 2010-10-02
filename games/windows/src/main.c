09197b41561d60dfa4ddfd3529f63a102940f6cc
Curtis (Programble) <programble@gmail.com>
2010-10-02 16:17:38 -0400

    Fun little keyboard loop in windows

--- games/windows/src/main.c
+++ games/windows/src/main.c
@@ -18,28 +18,33 @@
 
 #include <text.h>
 #include <timer.h>
+#include <keyboard.h>
 
 void main()
 {
-    cls();
-    sleep(100);
-    set_background_color(blue, false);
-    cls();
-    puts("\n\n\n\n\n\n\n\n");
-    for (int i = 0; i < 34; i++)
-        putch(' ');
-    set_background_color(gray, false);
-    set_foreground_color(blue, false);
-    puts(" Windows ");
-    set_background_color(blue, false);
-    set_foreground_color(gray, true);
-    puts("\n\n     A fatal exception 0E has occured at 0028:C0011E36 in UXD UMM(01) +\n");
-    puts("     00010E36. The current application will be terminated.\n\n");
-    puts("     *  Press any key to terminate the current application.\n");
-    puts("     *  Press CTRL+ALT+DEL again to restart your computer. You will\n");
-    puts("        lose any unsaved information in all applications.\n\n");
-    for (int i = 0; i < 27; i++)
-        putch(' ');
-    puts("Press any key to continue ");
-    while(1);
+    while (1)
+    {
+        set_background_color(black, false);
+        cls();
+        sleep(100);
+        set_background_color(blue, false);
+        cls();
+        puts("\n\n\n\n\n\n\n\n");
+        for (int i = 0; i < 34; i++)
+            putch(' ');
+        set_background_color(gray, false);
+        set_foreground_color(blue, false);
+        puts(" Windows ");
+        set_background_color(blue, false);
+        set_foreground_color(gray, true);
+        puts("\n\n     A fatal exception 0E has occured at 0028:C0011E36 in UXD UMM(01) +\n");
+        puts("     00010E36. The current application will be terminated.\n\n");
+        puts("     *  Press any key to terminate the current application.\n");
+        puts("     *  Press CTRL+ALT+DEL again to restart your computer. You will\n");
+        puts("        lose any unsaved information in all applications.\n\n");
+        for (int i = 0; i < 27; i++)
+            putch(' ');
+        puts("Press any key to continue ");
+        get_key_event();
+    }
 }
