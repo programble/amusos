2a2b21bf85a8f0607745aafcdce44b9c54f18399
Curtis (Programble) <programble@gmail.com>
2010-10-02 16:05:11 -0400

    Implemented half the keyboard API, not sure how I'm going to handle the other part...

--- lib/include/keyboard.h
+++ lib/include/keyboard.h
@@ -6,7 +6,7 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *
+ * 
  *  AmusOS is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,35 +16,34 @@
  *  along with AmusOS.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <text.h>
-#include <mm.h>
-#include <timer.h>
-#include <keyboard.h>
+#ifndef __KEYBOARD_H__
+#define __KEYBOARD_H__
 
-void main()
+#include <common.h>
+#include <syscall.h>
+
+/**
+ * Key event types.
+ */
+typedef enum
 {
-    const char text[23] = "This is the test game. ";
-    for (int i = 0; i < 23; i++)
-    {
-        putch(text[i]);
-        sleep(20);
-    }
-    /*while (1)
-    {
-        for (int i = 1; i < 16; i++)
-        {
-            set_cursor_height(i);
-            sleep(10);
-        }
-        for (int i = 16; i > 0; i--)
-        {
-            set_cursor_height(i);
-            sleep(10);
-        }
-        }*/
-    while (1)
-    {
-        KeyEvent key = get_key_event();
-        if (!key.type) putch(key.keychar);
-    }
-}
+    KeyEvent_down,
+    KeyEvent_up
+} KeyEvent_type;
+
+/**
+ * Information on a key event.
+ */
+typedef struct
+{
+    KeyEvent_type type;
+    int keycode;
+    char keychar;
+    bool shift;
+    bool ctrl;
+    bool alt;
+} KeyEvent;
+
+KeyEvent get_key_event();
+
+#endif
--- lib/src/keyboard.c
+++ lib/src/keyboard.c
@@ -6,7 +6,7 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *
+ * 
  *  AmusOS is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,35 +16,12 @@
  *  along with AmusOS.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <text.h>
-#include <mm.h>
-#include <timer.h>
-#include <keyboard.h>
+#include "keyboard.h"
 
-void main()
+KeyEvent get_key_event()
 {
-    const char text[23] = "This is the test game. ";
-    for (int i = 0; i < 23; i++)
-    {
-        putch(text[i]);
-        sleep(20);
-    }
-    /*while (1)
-    {
-        for (int i = 1; i < 16; i++)
-        {
-            set_cursor_height(i);
-            sleep(10);
-        }
-        for (int i = 16; i > 0; i--)
-        {
-            set_cursor_height(i);
-            sleep(10);
-        }
-        }*/
-    while (1)
-    {
-        KeyEvent key = get_key_event();
-        if (!key.type) putch(key.keychar);
-    }
+    KeyEvent event;
+    KeyEvent *p = &event;
+    syscall1(17, p);
+    return event;
 }
